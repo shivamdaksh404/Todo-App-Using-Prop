c26f672423fe4f044c60f227fb3535a22bf760f0
shivamdaksh404 <126654523+shivamdaksh404@users.noreply.github.com>
2023-06-15 11:31:31 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,58 +1,71 @@
 //Build a To-Do list app using React The app should allow users to add new tasks, mark them as complete, and delete them. Use React state to manage the list of tasks and their completion status. Use React props to pass data between components.
 
-import React,{useState} from 'react';
+import React, { useState,useEffect } from 'react';
 import './style.css';
-import Tasks from './Tasks.jsx'
+import Tasks from './Tasks.jsx';
 
 export default function App() {
-const [taskInput,setTaskInput] = useState("")
-const [tasks,setTasks] = useState([])
+  const [taskInput, setTaskInput] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-const handleAddTask =()=>{
-  if(taskInput.trim()==''){
-    alert('Please Add Task')
-  }else{
-    const newTask = {
-      name: taskInput,
-      completed: false,
-    };
-setTasks([...tasks,newTask])
-  }
-  setTaskInput("")
+useEffect(()=>{
+const data = JSON.parse(localStorage.getItem("tasks"))
+if(data){
+  setTasks(data)
 }
+},[])
 
-const taskCompleted =(index)=>{
-const updatedData= [...tasks]
-updatedData[index].completed = true;
-setTasks(updatedData)
-}
 
-const taskDeleted =(index)=>{
-const updatedData= [...tasks];
-updatedData.splice(index,1);
-setTasks(updatedData)
-}
+  const handleAddTask = () => {
+    if (taskInput.trim() == '') {
+      alert('Please Add Task');
+    } else {
+      const newTask = {
+        name: taskInput,
+        completed: false,
+      };
+      const updatedTask= [...tasks,newTask]
+      setTasks(updatedTask);
+      localStorage.setItem('tasks', JSON.stringify(updatedTask));
+    }
+    setTaskInput('');
+  };
+
+  const taskCompleted = (index) => {
+    const updatedData = [...tasks];
+    updatedData[index].completed = true;
+    setTasks(updatedData);
+    localStorage.setItem('tasks',JSON.stringify(updatedData))
+  };
+
+  const taskDeleted = (index) => {
+    const updatedData = [...tasks];
+    updatedData.splice(index, 1);
+    setTasks(updatedData);
+    localStorage.setItem('tasks',JSON.stringify(updatedData))
+  };
 
   return (
     <div className="todo-app">
       <h1>Todo List</h1>
-      <div className='task-input'>
-        <input type="text"
-        placeholder="Enter your task" 
-        value={taskInput} 
-        onChange={(e)=>setTaskInput(e.target.value)}
+      <div className="task-input">
+        <input
+          type="text"
+          placeholder="Enter your task"
+          value={taskInput}
+          onChange={(e) => setTaskInput(e.target.value)}
         />
         <button onClick={handleAddTask}>Add Task</button>
       </div>
       <div className="task-list">
-          {tasks.map((task,index)=><Tasks
-          index={index}
-          task={task}
-          taskCompleted={taskCompleted}
-          taskDeleted={taskDeleted}
-
-          />)}
-       
+        {tasks.map((task, index) => (
+          <Tasks
+            index={index}
+            task={task}
+            taskCompleted={taskCompleted}
+            taskDeleted={taskDeleted}
+          />
+        ))}
       </div>
     </div>
   );
